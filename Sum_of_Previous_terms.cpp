207ead841d2ab4c05a29d6be837b580be1fbb5bc
Rohit <rohitsav1305@gmail.com>
2020-10-09 23:20:13 +0530

    Sum_of_previous_term

---
+++ Sum_of_Previous_terms.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main(){
+    int n,t1 = 1, t2 = 2,curr=0;
+    cin >> n;
+    if(n==1 || n==2){
+        cout << n;
+    }
+    else if(n>30){
+        cout << "Invalid Input";
+    }
+    else{
+        for(int i = 3; i<=n; i++){
+            curr = t1 + t2;
+            t1=t2;
+            t2=curr;
+        }
+    cout << curr;
+    }
+    return 0;
+}

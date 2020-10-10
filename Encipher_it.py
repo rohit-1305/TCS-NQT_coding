a754e76dfe2ec44262a5cd9ef1db6210aab2ff38
Rohit <rohitsav1305@gmail.com>
2020-10-10 20:32:28 +0530

    Encipher_it

---
+++ Encipher_it.py
@@ -0,0 +1,11 @@
+import string
+uppercase = list(string.ascii_uppercase)
+lowercase = list(string.ascii_lowercase)
+letter = input()
+number = int(input())
+if letter in uppercase:
+    key = uppercase.index(letter)
+    print(uppercase[(key+number) % 26])
+elif letter in lowercase:
+    key = lowercase.index(letter)
+    print(lowercase[(key+number) % 26])

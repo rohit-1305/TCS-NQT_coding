920c860bf2975e3ed9c6ea700be33b79368c20da
Rohit <rohitsav1305@gmail.com>
2020-10-22 16:21:53 +0530

    Circumference of a circle

---
+++ Circumference_of_a_circle.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    float radius;
+    cin >> radius;
+    cout << fixed << setprecision(2) << 2 * 3.14 * radius;
+}
